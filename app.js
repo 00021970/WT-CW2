f2f8d70b800b601a4aa249fce01164deec00cf5d
00021970 <00021970id@gmail.com>
2025-04-07 17:16:59 +0500

    feature: add create.html for list creation and update app.js to handle routing
    - Update app.js with .get and .post for the list creation page
    - Add create.html to display a basic list creation form

--- app.js
+++ app.js
@@ -4,16 +4,32 @@ const path = require('path')
 
 const port = 3000;
 
+app.use(express.urlencoded({
+    extended: true
+}));
+
 //Homepage
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
 });
 
+//Create a list
+app.get('/create', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/html/create.html'));
+});
+
+app.post('/create', (req, res)=>{
+    let listname = req.body.listname;
+    let item = req.body.item;
+    console.log(listname, item);
+    res.redirect('/create')
+});
+
 //Specific to do list
 app.get('/:list', (req, res)=>{
     let listname = req.params.list;
-    console.log(listname)
-    res.sendFile(path.join(__dirname, 'public/html/list.html'))
+    console.log(listname);
+    res.sendFile(path.join(__dirname, 'public/html/list.html'));
 });
 
 app.listen(port, ()=>{
